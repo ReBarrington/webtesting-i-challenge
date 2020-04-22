e1c6da578beef6a5b727cc46b236dae47a66c3d8
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-22 14:32:36 -0400

    initial enhancer methods

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,32 @@ module.exports = {
   get,
 };
 
+// a success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement += 1
+  }
   return { ...item };
 }
 
+// a fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
 function fail(item) {
+  if (item.enhancement < 15) {
+    item.enhancement -= 5
+  } else if (item.enhancement > 15) {
+    let addedPenalty = item.enhancement - 15
+    item.enhancement -= (10 + addedPenalty)
+  }
   return { ...item };
 }
 
+// a repair(item) method that accepts an item object and returns a new item with the durability restored to 100.
 function repair(item) {
+  item.durability = 100
   return { ...item };
 }
 
+// stretch
 function get(item) {
   return { ...item };
 }
